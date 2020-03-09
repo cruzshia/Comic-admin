14db4ed199c633b6ca80601f68fa7770f9a38a8c
aCruzShia <cruz.shia@access-company.com>
2020-03-09 09:15:36 +0800

    [RA-1294][RA-1298] Sticky bar component (#38)
    * StickyBar component implementation
    
    * adjust unit test

---
+++ ui/src/components/StickyBar/StickyBar.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect, useRef } from 'react'
+import { fromEvent } from 'rxjs'
+import Box from '@material-ui/core/Box'
+import RootRef from '@material-ui/core/RootRef'
+import { borderColorLight, topOffset } from '@src/common/styles'
+
+export default function StickyBar({ top, children }: { top: number; children: React.ReactNode }) {
+  const [visible, setVisible] = useState<boolean>(false)
+  const domRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    const displayPos = top + (domRef.current!.clientHeight ?? 0)
+    const subscription = fromEvent(document, 'scroll').subscribe(() => {
+      if (document.documentElement.scrollTop > displayPos) {
+        setVisible(true)
+        return
+      }
+      setVisible(false)
+    })
+    return () => subscription.unsubscribe()
+  }, [top])
+
+  return (
+    <RootRef rootRef={domRef}>
+      <Box
+        data-testid='sticky-bar'
+        display='flex'
+        alignItems='center'
+        justifyContent='space-between'
+        width='100%'
+        maxWidth={1180}
+        padding='12px 30px'
+        position='fixed'
+        top={topOffset + top}
+        bgcolor='#FFFFFF'
+        borderBottom={`1px solid ${borderColorLight}`}
+        zIndex={visible ? 1202 : -1}
+      >
+        {children}
+      </Box>
+    </RootRef>
+  )
+}
---
+++ ui/src/components/StickyBar/__test__/StickyBar.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import { withAllProvider } from '@src/utils/__test__/providers'
+import { topOffset } from '@src/common/styles'
+import StickyBar from '../StickyBar'
+
+describe('StickyBar component test', () => {
+  it('Renders correctly', () => {
+    const { container } = render(withAllProvider(<StickyBar top={0}>123</StickyBar>))
+    expect(container).toBeInTheDocument()
+  })
+
+  it('Shows up when scroll to position', () => {
+    const visibleTop = 10
+    const { getByTestId } = render(withAllProvider(<StickyBar top={visibleTop}>123</StickyBar>))
+    const element = getByTestId('sticky-bar')
+    let style = getComputedStyle(element)
+    expect(Number(style.zIndex)).toBeLessThan(0)
+
+    document.documentElement.scrollTop = topOffset + visibleTop + 1
+    fireEvent.scroll(document)
+
+    style = getComputedStyle(element)
+    expect(Number(style.zIndex)).toBeGreaterThanOrEqual(1)
+  })
+})
