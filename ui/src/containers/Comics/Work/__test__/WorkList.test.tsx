4a38f6038d41ac56197b55b1a70c2f26bb02efec
aCruzShia <cruz.shia@access-company.com>
2020-03-31 09:24:34 +0800

    work list page layout test (#82)

--- ui/src/containers/Comics/Work/__test__/WorkList.test.tsx
+++ ui/src/containers/Comics/Work/__test__/WorkList.test.tsx
@@ -15,4 +15,38 @@ describe('WorkList test', () => {
     )
     expect(container).toBeInTheDocument()
   })
+
+  it('Renders Search & list block', () => {
+    const mockDatum = {
+      image: <img alt='' />,
+      workID: 'WORK_ROOKIE000014751',
+      title: 'クラスメイトの田中さんはすごく怖い',
+      releaseDate: '2020-01-21 16:34:00',
+      category: 'コミックス',
+      episodeCategory: 'オリジナル連載',
+      updateFrequency: '毎週月月金曜日に更新'
+    }
+    const workList = []
+    for (let i = 0; i <= 3; i++) {
+      workList.push({ ...mockDatum, workID: `WORK_ROOKIE00001475${i}` })
+    }
+
+    const { getByTestId, getAllByTestId } = render(
+      withAllProvider(
+        <WorkContext.Provider value={{ workList, workTotal: 0 }}>
+          <WorkList />
+        </WorkContext.Provider>
+      )
+    )
+
+    expect(getByTestId('breadcrumbs')).toBeInTheDocument()
+    const allBreadcrumbs = getAllByTestId('breadcrumbs-text')
+    expect(allBreadcrumbs).toHaveLength(2)
+    expect(allBreadcrumbs[allBreadcrumbs.length - 1].textContent).toBe(getByTestId('content-header-title').textContent)
+
+    expect(getByTestId('search_filter')).toBeInTheDocument()
+
+    expect(getByTestId('list-table')).toBeInTheDocument()
+    expect(getAllByTestId('list-table-row')).toHaveLength(4)
+  })
 })
