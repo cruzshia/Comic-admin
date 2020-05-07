822061db4a1aed7ed6a9a29ce255f6fe450b4f1c
aEricLiao <eric.liao@access-company.com>
2020-05-07 17:55:47 +0800

    [RA-2932][RA-2938] Render author list searchForm & tableList (#253)
    * add comics author list rendering test
    
    * pause for merge
    
    * implement searchForm testing
    
    * change folder order
    
    * use isRightSearchBtn
    
    * remove blank line
    
    * implement searchForm testing
    
    * add rendering list testing
    
    * remove mockData test

--- ui/cypress/integration/comics/author/authorList.spec.js
+++ ui/cypress/integration/comics/author/authorList.spec.js
@@ -1,10 +1,12 @@
 context('Author List', () => {
   const targetRoute = '/#/comics/author'
   const targetTab = '著者管理'
+
   beforeEach(() => {
     cy.visit(targetRoute)
     cy.fixture('headerTabs.json').as('headerTabs')
   })
+
   it('Click tab route correctly', function() {
     cy.visit('/')
     cy.findAllByTestId('header-tab')
@@ -43,7 +45,46 @@ context('Author List', () => {
       .should('be.rightSearchBtn')
   })
 
-  it('Show pagination correctly', () => {
-    cy.findByTestId('pagination').should('exist')
+  it('Show Search Form correctly', () => {
+    cy.findByTestId('search_filter')
+      .children('form')
+      .within(() => {
+        cy.findAllByTestId('search_filter_item').should('have.length', 1)
+        cy.findByTestId('search-filter-item-label').should('contain', '著者（ID）')
+        cy.findByTestId('search_input').within($input => {
+          cy.findByTestId('search_icon').should('be.exist')
+          cy.wrap($input)
+            .children('input')
+            .should('have.attr', 'placeholder', 'フリーワードで検索')
+        })
+      })
+  })
+
+  it('Show Table List correctly ', () => {
+    const tableColumnNum = 4
+    cy.findAllByTestId('list-table')
+      .should('have.length', 1)
+      .within(() => {
+        cy.findByTestId('list-table-pagination').should('contain', '全1000件表示（1~100件目を表示）')
+
+        cy.findByTestId('table-head-row')
+          .children('th')
+          .should('have.length', tableColumnNum)
+          .first()
+          .should('contain', '作成日時')
+          .and('is.sortByTableHeadCell', { sorting: true })
+          .next()
+          .should('contain', 'ID')
+          .next()
+          .should('contain', '著者名')
+
+        cy.findAllByTestId('list-table-row').each($row => {
+          cy.wrap($row)
+            .children('td')
+            .should('have.length', tableColumnNum)
+        })
+
+        cy.findByTestId('pagination').should('be.exist')
+      })
   })
 })
