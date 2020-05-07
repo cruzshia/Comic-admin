e49daa21a38eb8a0427239186f28c7be58d81c58
Yi-Shin, Ho <yishin.ho@access-company.com>
2020-05-07 15:13:34 +0800

    [RA-2928] Content search form layout test (#251)

--- ui/cypress/integration/comics/contents/contentList.spec.js
+++ ui/cypress/integration/comics/contents/contentList.spec.js
@@ -48,6 +48,118 @@ context('ContentList', () => {
       .should('be.rightSearchBtn')
   })
 
+  it('Renders correct search form', () => {
+    cy.findByTestId('search_filter').should('be.exist')
+
+    cy.findByTestId('search-filter-items-left').as('filterLeft')
+
+    cy.get('@filterLeft')
+      .findAllByTestId('search_filter_item')
+      .first()
+      .then($item => {
+        cy.wrap($item).as('searchItem')
+        cy.get('@searchItem')
+          .findByTestId('search-filter-item-label')
+          .contains('コンテンツ（ID）')
+        cy.get('@searchItem')
+          .findByTestId('search_input')
+          .should('be.exist')
+      })
+
+    cy.get('@filterLeft')
+      .findAllByTestId('search_filter_item')
+      .eq(1)
+      .then($item => {
+        cy.wrap($item).as('searchItem')
+        cy.get('@searchItem')
+          .findByTestId('search-filter-item-label')
+          .contains('著者')
+        cy.get('@searchItem')
+          .findByTestId('search_input')
+          .should('be.exist')
+      })
+
+    cy.get('@filterLeft')
+      .findAllByTestId('search_filter_item')
+      .eq(2)
+      .then($item => {
+        cy.wrap($item).as('searchItem')
+        cy.get('@searchItem')
+          .findByTestId('search-filter-item-label')
+          .contains('作品（ID）')
+        cy.get('@searchItem')
+          .findByTestId('search_input')
+          .should('be.exist')
+      })
+
+    cy.get('@filterLeft')
+      .findAllByTestId('search_filter_item')
+      .last()
+      .then($item => {
+        cy.wrap($item).as('searchItem')
+        cy.get('@searchItem')
+          .findByTestId('search-filter-item-label')
+          .contains('アプリID')
+        cy.get('@searchItem')
+          .findByTestId('select')
+          .should('be.exist')
+      })
+
+    cy.findByTestId('search-filter-items-right').as('filterRight')
+
+    cy.get('@filterRight')
+      .findAllByTestId('search_filter_item')
+      .first()
+      .then($item => {
+        cy.wrap($item).as('searchItem')
+        cy.get('@searchItem')
+          .findByTestId('search-filter-item-label')
+          .contains('コンテンツ種別')
+        cy.get('@searchItem')
+          .findByTestId('select')
+          .should('be.exist')
+      })
+
+    cy.get('@filterRight')
+      .findAllByTestId('search_filter_item')
+      .eq(1)
+      .then($item => {
+        cy.wrap($item).as('searchItem')
+        cy.get('@searchItem')
+          .findByTestId('search-filter-item-label')
+          .contains('広告ユニット')
+        cy.get('@searchItem')
+          .findByTestId('select')
+          .should('be.exist')
+      })
+
+    cy.get('@filterRight')
+      .findAllByTestId('search_filter_item')
+      .eq(2)
+      .then($item => {
+        cy.wrap($item).as('searchItem')
+        cy.get('@searchItem')
+          .findByTestId('search-filter-item-label')
+          .contains('配信開始日時')
+        cy.get('@searchItem')
+          .findByTestId('time_span_input')
+          .should('be.exist')
+      })
+
+    cy.get('@filterRight')
+      .findAllByTestId('search_filter_item')
+      .last()
+      .then($item => {
+        cy.wrap($item).as('searchItem')
+        cy.get('@searchItem')
+          .findByTestId('search-filter-item-label')
+          .contains('配信終了日時')
+        cy.get('@searchItem')
+          .findByTestId('time_span_input')
+          .should('be.exist')
+      })
+  })
+
   it('Renders pagination', () => {
     cy.findByTestId('pagination').should('be.exist')
   })
