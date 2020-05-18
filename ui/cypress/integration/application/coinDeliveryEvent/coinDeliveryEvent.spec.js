78b5beaffbf4b466b6970338fabae428dffba5ac
aCruzShia <cruz.shia@access-company.com>
2020-05-18 15:47:19 +0800

    [RA-3388][RA-3389] coin delivery search form and list table e2e test (#298)
    * implement coin delivery search form and listable e2e
    
    * add contentheader e2e test case

--- ui/cypress/integration/application/coinDeliveryEvent/coinDeliveryEvent.spec.js
+++ ui/cypress/integration/application/coinDeliveryEvent/coinDeliveryEvent.spec.js
@@ -30,4 +30,84 @@ context('Coin Delivery Event List', () => {
       .parent()
       .should('be.sideTabSelected')
   })
+
+  it('Shows correct page title and breadcrumb', function() {
+    const pageTitle = 'コイン付与イベント一覧'
+    cy.findByTestId(this.testIds.contentHeaderTitle).should('contain', pageTitle)
+    cy.findByTestId(this.testIds.breadcrumbs).should('contain', `${this.headerTabs.application}>${pageTitle}`)
+  })
+
+  it('Shows correct content header button', function() {
+    cy.findByTestId(this.testIds.contentHeaderButtons).should('contain', 'コイン付与イベントを登録')
+  })
+
+  it('Renders correct search form', function() {
+    const ITEM_LABEL_SELECTOR = `[data-testid=${this.testIds.searchFilter.itemLabel}]`
+    cy.findByTestId(this.testIds.searchFilter.id).should('be.exist')
+    cy.findByTestId(this.testIds.searchFilter.itemsLeft)
+      .findAllByTestId(this.testIds.searchFilter.item)
+      .first()
+      .should($item => {
+        expect($item.find(ITEM_LABEL_SELECTOR)).have.text('イベント名')
+        expect($item.find(`[data-testid=${this.testIds.inputs.text}]`)).to.be.exist
+      })
+      .next()
+      .should($item => {
+        expect($item.find(ITEM_LABEL_SELECTOR)).have.text('配信開始日時')
+        expect($item.find(`[data-testid=${this.testIds.inputs.text}]`)).to.be.exist
+      })
+
+    cy.findByTestId('search-filter-items-right')
+      .findByTestId(this.testIds.searchFilter.item)
+      .should($item => {
+        expect($item.find(ITEM_LABEL_SELECTOR)).have.text('イベント種別')
+        expect($item.find(`[data-testid=${this.testIds.inputs.select}]`)).to.be.exist
+      })
+  })
+
+  it('Renders correct search button', function() {
+    cy.findByTestId(this.testIds.searchFilter.buttons)
+      .children('button')
+      .should('be.rightSearchBtn')
+  })
+
+  it('Renders correct list table', function() {
+    const tableColNum = 6
+    cy.findByTestId(this.testIds.listTable.id)
+      .as('listTable')
+      .should('be.exist')
+
+    cy.get('@listTable')
+      .findByTestId(this.testIds.listTable.pageInfo)
+      .should('be.exist')
+
+    cy.get('@listTable')
+      .findByTestId(this.testIds.listTable.tableHead)
+      .children('th')
+      .should('have.lengthOf', tableColNum)
+      .first()
+      .should('have.text', 'イベントID')
+      .next()
+      .should('have.text', 'イベント名')
+      .next()
+      .should('have.text', 'イベント種別')
+      .next()
+      .should('have.text', 'イベント開始日時')
+      .and('be.sortableHeadCell', { sorting: true })
+      .next()
+      .should('have.text', 'イベント終了日時')
+      .and('be.sortableHeadCell')
+      .click()
+      .should('be.sortableHeadCell', { sorting: true })
+
+    cy.get('@listTable')
+      .findAllByTestId(this.testIds.listTable.tableRow)
+      .first()
+      .findAllByTestId(this.testIds.listTable.tableRowCell)
+      .should('have.lengthOf', tableColNum)
+  })
+
+  it('Renders pagination', function() {
+    cy.findByTestId(this.testIds.pager).should('be.exist')
+  })
 })
