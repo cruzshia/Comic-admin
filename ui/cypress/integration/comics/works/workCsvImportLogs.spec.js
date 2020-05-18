acdd3078cf3e6553681c44917d5febb3803babb4
aCruzShia <cruz.shia@access-company.com>
2020-05-18 16:04:51 +0800

    [RA-3425] Work import logs e2e test (#301)
    * work import logs e2e test
    
    * remove sort feature in work import log page

---
+++ ui/cypress/integration/comics/works/workCsvImportLogs.spec.js
@@ -0,0 +1,72 @@
+/// <reference types="cypress" />
+
+context('Work import log page', () => {
+  const targetRoute = '/#/comics/work/import_logs'
+  beforeEach(() => {
+    cy.visit(targetRoute)
+    cy.fixture('headerTabs.json').as('headerTabs')
+    cy.fixture('testIds.json').as('testIds')
+  })
+
+  it('Click tab route correctly', function() {
+    cy.visit('/')
+    cy.findAllByTestId(this.testIds.headerTab)
+      .contains(this.headerTabs.comic)
+      .click()
+    cy.findAllByTestId(this.testIds.sidebarTab)
+      .contains('作品管理')
+      .click()
+    cy.findAllByTestId(this.testIds.button.normal)
+      .contains('CSV登録ログ')
+      .click()
+      .url()
+      .should('include', targetRoute)
+  })
+
+  it('Shows correct page title and breadcrumb', function() {
+    const pageTitle = '作品CSV登録ログ'
+    cy.findByTestId('content-header-title').should('contain', pageTitle)
+    cy.findByTestId('breadcrumbs').should('contain', `${this.headerTabs.comic}>作品一覧>${pageTitle}`)
+    cy.findByTestId('breadcrumbs-link').should('have.attr', 'href', '#/comics/work')
+  })
+
+  it('Renders correct log list table', function() {
+    const tableColNum = 7
+    cy.findByTestId(this.testIds.listTable.id)
+      .as('listTable')
+      .should('be.exist')
+
+    cy.get('@listTable')
+      .findByTestId(this.testIds.listTable.pageInfo)
+      .should('be.exist')
+
+    cy.get('@listTable')
+      .findByTestId(this.testIds.listTable.tableHead)
+      .children('th')
+      .should('have.lengthOf', tableColNum)
+      .first()
+      .should('have.text', '作成日時')
+      .next()
+      .should('have.text', '予約日時')
+      .next()
+      .should('have.text', '開始日時')
+      .next()
+      .should('have.text', '更新日時')
+      .next()
+      .should('have.text', 'ファイル名')
+      .next()
+      .should('have.text', '状態')
+      .next()
+      .should('have.text', '詳細')
+
+    cy.get('@listTable')
+      .findAllByTestId(this.testIds.listTable.tableRow)
+      .first()
+      .findAllByTestId(this.testIds.listTable.tableRowCell)
+      .should('have.lengthOf', tableColNum)
+  })
+
+  it('Renders pagination', function() {
+    cy.findByTestId(this.testIds.pager).should('be.exist')
+  })
+})
--- ui/src/containers/Comics/Work/components/WorkImportLogs.tsx
+++ ui/src/containers/Comics/Work/components/WorkImportLogs.tsx
@@ -3,7 +3,7 @@ import { useIntl } from 'react-intl'
 import ContentHeader, { Breadcrumb } from '@src/components/ContentHeader/ContentHeader'
 import CsvImportLogTable from '@src/components/table/CsvImportLogTable'
 import { DownloadBlock, FailedMsg } from '@src/components/styled'
-import { useSort, usePaging } from '@src/hooks'
+import { usePaging } from '@src/hooks'
 import commonMessages from '@src/messages'
 import messages from '../messages'
 import { BREADCRUMBS } from '../constants'
@@ -13,7 +13,6 @@ export default function WorkImportLogs() {
   const { formatMessage } = useIntl()
   const { importLogList, logTotal } = useContext(WorkContext)
   const { onGetCsvLogList } = useContext(ActionContext)
-  const { sortBy, handleSort } = useSort('createAt')
   const { pagination, handlePageChange } = usePaging({ total: logTotal })
 
   useEffect(() => {
@@ -29,31 +28,19 @@ export default function WorkImportLogs() {
     [formatMessage]
   )
 
-  const displayList = importLogList
-    .map(({ id, ...log }) => ({
-      id,
-      data: {
-        ...log,
-        filename: <DownloadBlock filename={log.filename} />,
-        detail:
-          log.status === 'failure' ? <FailedMsg msg={formatMessage(commonMessages.errorAsyncFailed)} /> : log.detail
-      }
-    }))
-    .sort(
-      (a, b) => (new Date(a.data[sortBy.key]).getTime() - new Date(b.data[sortBy.key]).getTime()) * sortBy.multiplier
-    )
+  const displayList = importLogList.map(({ id, ...log }) => ({
+    id,
+    data: {
+      ...log,
+      filename: <DownloadBlock filename={log.filename} />,
+      detail: log.status === 'failure' ? <FailedMsg msg={formatMessage(commonMessages.errorAsyncFailed)} /> : log.detail
+    }
+  }))
 
   return (
     <>
       <ContentHeader breadcrumbList={breadcrumbList} titleText={formatMessage(messages.worksCsvImportLog)} />
-      <CsvImportLogTable
-        onSort={handleSort}
-        dataList={displayList}
-        pagination={pagination}
-        onPageChange={handlePageChange}
-        sortBy={sortBy.key}
-        sortOrder={sortBy.order}
-      />
+      <CsvImportLogTable dataList={displayList} pagination={pagination} onPageChange={handlePageChange} />
     </>
   )
 }
