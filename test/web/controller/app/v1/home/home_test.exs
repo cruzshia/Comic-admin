ec8e766fb8b3669598118e8f01fad6ad242e30af
Harper Chuang <harper.chuang@access-company.com>
2020-06-05 16:16:44 +0800

    [RA-1994] Fix home controller test (#418)

--- test/web/controller/app/v1/home/home_test.exs
+++ test/web/controller/app/v1/home/home_test.exs
@@ -1,28 +1,27 @@
-# TODO Fix this test
-defmodule RaiseServer.Controller.App.V1.HomeTest do
+defmodule RaiseServer.Controller.App.V1.Home.ShowTest do
   use RaiseServer.RepoCase
 
-  alias RaiseServer.AppsFactory
+  alias RaiseServer.{AppsFactory, HomeData}
 
   @path "/api/app/v1/home"
 
   describe "get/1" do
     setup do
       app = AppsFactory.insert(:app)
-      [app: app, header: %{ "x-raise-aid" => app.app_id_token}]
+      [app: app, header: %{"x-raise-aid" => app.app_id_token}]
     end
 
-    # test "returns home sections", %{app: app, header: header} do
-    #   HomeData.create_resources(app)
+    test "returns home sections", %{app: app, header: header} do
+      HomeData.create_resources(app)
 
-    #   %{"sections" => _orig_sections} =
-    #     HomeData.app_screen_setting_json_str()
-    #     |> :jsx.decode(return_maps: true)
+      %{"sections" => original_sections} =
+        HomeData.app_screen_setting_json_str()
+        |> :jsx.decode(return_maps: true)
 
-    #   assert %{status: 200, body: body} = Req.get(@path, header)
-    #   assert %{"sections" => _sections} = Jason.decode!(body)
-    #   #Enum.scan(sections, orig_sections, &validate_section/2)
-    # end
+      assert %{status: 200, body: body} = Req.get(@path, header)
+      assert %{"sections" => sections} = Jason.decode!(body)
+      Enum.scan(sections, original_sections, &validate_section_order/2)
+    end
 
     test "returns ResourceNotFound if the screen setting is nothing", %{header: header} do
       res = Req.get(@path, header)
@@ -30,8 +29,8 @@ defmodule RaiseServer.Controller.App.V1.HomeTest do
     end
   end
 
-  #defp validate_section(res_section, [expect_section | tail]) do
-    #assert res_section == expect_section
-    #tail
-  #end
+  defp validate_section_order(res_section, [expect_section | tail]) do
+    assert res_section["type"] == expect_section["type"]
+    tail
+  end
 end
